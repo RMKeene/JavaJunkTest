ee80b410332b82f6cc5d0bc86c42c3eed2dae8bd
rkeene <rmkeene@gmail.com>
2022-06-22 14:51:47 -0600

    Added in some array and a loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,15 @@
+import java.util.*;
+
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        
+        var numbers = Arrays.asList(11, 22, 33, 44,
+        55, 66, 77, 88,
+        99, 100);
+
+        numbers.forEach(number
+                        -> System.out.print(
+                            number + " "));
     }
 }
